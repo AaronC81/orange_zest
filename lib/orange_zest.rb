58313b8ec1b1c567e15ba36889a86a2290c1c357
Aaron Christiansen <aaronc20000@gmail.com>
2022-08-27 21:43:44 +0100

    Initial commit

---
+++ lib/orange_zest.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OrangeZest; end
+
+require_relative "orange_zest/version"
---
+++ spec/orange_zest_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe OrangeZest do
+  it "has a version number" do
+    expect(OrangeZest::VERSION).not_to be nil
+  end
+end
